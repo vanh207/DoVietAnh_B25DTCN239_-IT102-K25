e590a9557cccf62cc9f0b15576ae21adb59381de
vanh207 <161843328+vanh207@users.noreply.github.com>
2025-10-20 15:37:41 +0700

    Update BTVN 1.c

--- Session 3/BTVN 1.c
+++ Session 3/BTVN 1.c
@@ -3,9 +3,10 @@ int main() {
     char name[100];
     
     printf("Vui long nhap ten cua ban: ");
-    scanf("%99[^\n]", name); 
+    scanf("%s", name); 
     
     printf("xin chao %s\n", name);
     
     return 0;
 }
+
