d1fc218a9f92c3c6c2fbde927bb6271ba4d61a42
vanh207 <161843328+vanh207@users.noreply.github.com>
2025-10-27 22:57:28 +0700

    Update BTVN 9.c

--- Session 4/BTVN 9.c
+++ Session 4/BTVN 9.c
@@ -1,21 +1,41 @@
 #include <stdio.h>
-int main(){
-	int number, number2,number3;
-	printf("ngay = ");
-	scanf("%d",&number2);
-	printf("thang = ");
-	scanf("%d",&number);
-	printf("nam = ");
-	scanf("%d",&number3);
-	if(number2<=29 && number==2 && number3%4==0 && number3%100!=0||number3%400==0){
-		printf("ngay thang nam hop le\n");
-	}else if(number2<=31 && number==1 || number==3 || number== 5|| number== 7|| number==8|| number==10|| number==12 ){
-		printf("ngay thang nam hop le\n");
-	}else if(number2<=30 && number==4 || number==6 || number== 9|| number== 11){
-		printf("ngay thang nam hop le\n");
-    }else{
-    	printf("ngay thang nam khong hop le");
-	}
-	return 0;
-	
+int main() {
+    int day, month, year;
+    
+    printf("Nhap ngay: ");
+    scanf("%d", &day);
+    printf("Nhap thang: ");
+    scanf("%d", &month);
+    printf("Nhap nam: ");
+    scanf("%d", &year);
+    
+    int valid = 1;
+    
+    if (month < 1 || month > 12) {
+        valid = 0;
+    } else {
+        switch (month) {
+            case 1: case 3: case 5: case 7: case 8: case 10: case 12:
+                if (day < 1 || day > 31) valid = 0;
+                break;
+            case 4: case 6: case 9: case 11:
+                if (day < 1 || day > 30) valid = 0;
+                break;
+            case 2:
+                if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
+                    if (day < 1 || day > 29) valid = 0;
+                } else {
+                    if (day < 1 || day > 28) valid = 0;
+                }
+                break;
+        }
+    }
+    
+    if (valid) {
+        printf("Ngay thang nam hop le\n");
+    } else {
+        printf("Ngay thang nam khong hop le\n");
+    }
+    
+    return 0;
 }
