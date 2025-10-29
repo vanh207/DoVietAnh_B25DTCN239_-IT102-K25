70392b27a43e6cef12515414897d5d353904f040
vanh207 <161843328+vanh207@users.noreply.github.com>
2025-10-29 08:57:32 +0700

    Update BTVN 10.c

--- Session 5/BTVN 10.c
+++ Session 5/BTVN 10.c
@@ -1,31 +1,20 @@
 #include <stdio.h>
-
-int main(){
-	float tiendien, sodien,tienbacthang;
-	int loai;
-	printf("nhap so dien tieu thu trong thang: ");
-	scanf("%f",&sodien);
-	printf("loai ho tieu dung (1->3) ");
-	scanf("%d",&loai);
-	if(sodien<=50){
-		tienbacthang=sodien*1.500;	
-	}else if(50<sodien<=100){
-		tienbacthang=sodien*2.000;
-	}else if(100<sodien<=200){
-		tienbacthang=sodien*2.500;
-	}else{
-		tienbacthang=sodien*3.000;
-	}
-	switch(loai){
-		case 1:
-			printf("tong so tien la %.3f",tienbacthang+tienbacthang*0.05);
-			break;
-		case 2:
-			printf("tong so tien la %.3f",tienbacthang+tienbacthang*0.1);
-			break;
-		case 3:
-			printf("tong so tien la %.3f",tienbacthang+tienbacthang*0.08);
-			break;		
-	}
-	return 0;	
+int main() {
+    float electricityUsage, electricityConsumption, additionFee, totalBi
+    int cosmumerType;
+    printf("Enter consumed electricity (kWh): ");
+    scanf("%f", &kWh);
+    printf("Enter type (1: Household, 2: Business, 3: Factory): ");
+    scanf("%d", &type);
+    cost = (kWh > 200) ? (50*1500 + 50*2000 + 100*2500 + (kWh - 200)*3000) :
+           (kWh > 100) ? (50*1500 + 50*2000 + (kWh - 100)*2500) :
+           (kWh > 50)  ? (50*1500 + (kWh - 50)*2000) :
+                         (kWh * 1500);
+    feeRate = (type == 1) ? 0.05 :
+              (type == 2) ? 0.10 :
+              (type == 3) ? 0.08 : -1;
+    (feeRate < 0) ? printf("Invalid type!\n") :
+    (fee = cost * feeRate,total = cost + fee,
+        printf("\nTotal cost = %.0f + %.0f = %.0f VND\n", cost, fee, total));
+    return 0;
 }
